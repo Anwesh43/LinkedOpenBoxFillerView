f1f8f3c661206f3d7c6743a04fd5b8f255c25611
Anwesh43 <anweshthecool0@gmail.com>
2020-09-17 02:50:54 +0530

    adding OpenBoxFillerView class

--- openboxfillerview/src/main/java/com/anwesh/uiprojects/openboxfillerview/OpenBoxFillerView.kt
+++ openboxfillerview/src/main/java/com/anwesh/uiprojects/openboxfillerview/OpenBoxFillerView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawOBFNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawOpenBoxFiller(scale, w, h, paint)
 }
+
+class OpenBoxFillerView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
