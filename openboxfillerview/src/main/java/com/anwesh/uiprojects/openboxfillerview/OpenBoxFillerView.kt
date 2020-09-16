648cf5be9bae5ac3d1be4356ec719d48828d6dd7
Anwesh43 <anweshthecool0@gmail.com>
2020-09-17 02:31:23 +0530

    adding constants and libraries

--- openboxfillerview/src/main/java/com/anwesh/uiprojects/openboxfillerview/OpenBoxFillerView.kt
+++ openboxfillerview/src/main/java/com/anwesh/uiprojects/openboxfillerview/OpenBoxFillerView.kt
@@ -3,3 +3,26 @@ package com.anwesh.uiprojects.openboxfillerview
 /**
  * Created by anweshmishra on 17/09/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#4CAF50",
+        "#FF5722",
+        "#3F51B5",
+        "#00BCD4"
+).map({Color.parseColor(it)}).toTypedArray()
+val lines : Int = 3
+val parts : Int = lines + 1
+val scGap : Float = 0.02f / (parts + 1)
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.8f
+val delay : Long = 20
+val gapDeg : Float = 90f
